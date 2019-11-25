019e9c978f4aeef9e56c8e20b12e76e9bed5b415
Johny Marques <johnymbr@gmail.com>
2019-11-25 15:11:55 -0300

    inclusao de novo grafico Mes x Valor.

--- trab-rec-pat/trab-rec-pat.py
+++ trab-rec-pat/trab-rec-pat.py
@@ -2,6 +2,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use('ggplot')
 
 df = pd.read_csv('cte2018.csv', sep=';')
-print(df.head())
+
+# Agrupar por "Mercadoria grupo"
+# df["Mercadoria grupo"].value_counts().plot.barh()
+# plt.show()
+
+# Agrupar os valores de conhecimento por Mes.
+# df['date'] = pd.to_datetime(df['Data emissão'])
+# df.groupby(df['date'].dt.strftime('%m'))['Total do conhec.'].sum().plot()
+# plt.ticklabel_format(style='plain', axis='y')
+# plt.grid('on', which='minor', axis='x')
+# plt.grid('off', which='major', axis='x')
+# plt.xlabel('Meses')
+# plt.ylabel('Valores')
+# plt.show()
+
+print(df.groupby(df['date'].dt.strftime('%m'))['Total do conhec.'].sum())
