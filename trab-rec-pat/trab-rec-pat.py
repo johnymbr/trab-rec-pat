143762c8487e7691ccc462ab1a12428730f1d8a3
Johny Marques <johnymbr@gmail.com>
2019-11-26 15:12:35 -0300

    inclusao de novos graficos.

--- trab-rec-pat/trab-rec-pat.py
+++ trab-rec-pat/trab-rec-pat.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 plt.style.use('ggplot')
+pd.options.display.float_format = '{:.2f}'.format
 
 df = pd.read_csv('cte2018.csv', sep=';')
 
@@ -20,4 +21,28 @@ df = pd.read_csv('cte2018.csv', sep=';')
 # plt.ylabel('Valores')
 # plt.show()
 
-print(df.groupby(df['date'].dt.strftime('%m'))['Total do conhec.'].sum())
+# Agrupar os valores por "UF da entrega"
+# ax1 = df.groupby(["UF da entrega"])["Peso carregado"].agg('sum').plot(color='blue', grid=True, label='Peso', kind="bar")
+# ax2 = df.groupby(["UF da entrega"])["Total do conhec."].agg('sum').plot(color='red', secondary_y=True, label='Valor', kind="bar")
+#
+# ax1.legend(loc=1)
+# ax1.ticklabel_format(style='plain', axis='y')
+# ax1.set_ylabel('Peso')
+# ax2.legend(loc=2)
+# ax2.ticklabel_format(style='plain', axis='y')
+# ax2.set_ylabel('Valor')
+#
+# plt.grid('on', which='minor', axis='x')
+# plt.grid('off', which='major', axis='x')
+# plt.xlabel('Estados Entrega')
+# plt.show()
+
+# Agrupar os valores por "Mes" e "UF da entrega"
+df['date'] = pd.to_datetime(df['Data emissão'])
+ax1 = df.groupby([df['date'].dt.strftime('%m'), "UF da entrega"])["Peso carregado"].agg('sum').plot(color='blue', grid=True, label='Peso', kind="bar")
+plt.ticklabel_format(style='plain', axis='y')
+plt.grid('on', which='minor', axis='x')
+plt.grid('off', which='major', axis='x')
+plt.xlabel('Mes / UF')
+plt.ylabel('Peso')
+plt.show()
